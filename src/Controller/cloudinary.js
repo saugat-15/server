bc5db93f89983b7f801836b742c15bd7bf956906
grouptag-saugat <saugatgiri15@gmail.com>
2023-02-04 15:50:05 +1100

    version updated cloudinary

--- src/Controller/cloudinary.js
+++ src/Controller/cloudinary.js
@@ -1,4 +1,4 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,4 +6,4 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary
+module.exports = cloudinary;
